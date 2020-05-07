888b905d9f8a07d73a44af94daae0af5a22b0c66
Jocelyn Gallardo <54326467+jocelyngallardo@users.noreply.github.com>
2020-05-07 15:45:34 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,6 +8,9 @@ app.debug = False #Change this to False for production
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/content')
+def content():
+    return render_template('content.html')
 
 if __name__ == '__main__':
     app.run()
